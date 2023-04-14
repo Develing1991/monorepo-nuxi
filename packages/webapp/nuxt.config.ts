fed9e945b1e301faf21a5e2205812753cf780ecf
shlee ⚡️ <completed0728@gmail.com>
2023-04-14 22:39:55 +0000

    recomended extensions add

--- packages/webapp/nuxt.config.ts
+++ packages/webapp/nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["~/assets/tailwind.css"],
-  modules: ["@develing1991/design/nuxt"],
+  css: ['~/assets/tailwind.css'],
+  modules: ['@develing1991/design/nuxt'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-});
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+})
