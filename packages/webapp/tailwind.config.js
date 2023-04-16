d2fa91ba2c990533169565ad58fb68f57daca5e7
shlee ⚡️ <completed0728@gmail.com>
2023-04-16 13:44:31 +0000

    change tsconfig include

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@develing1991/design/**/*.vue",
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
+    './node_modules/@develing1991/design/**/*.vue',
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
