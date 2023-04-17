eaba5b60865b70dea9551594493c813b87fded9e
Develing1991 <completed0728@gmail.com>
2023-04-17 18:22:50 +0900

    props 타입  {props:Props}로 테스트 코드

---
+++ packages/design/components/button/ButtonSetup.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { Props } from '@/types/Button'
+
+const { props: { label, bgColor } } = withDefaults(defineProps<{ props: Props }>(), {
+  props: () => ({
+    label: 'Button',
+    bgColor: 'bg-green-400',
+    reverse: false,
+  }),
+})
+
+const backgroundColor = computed(() => bgColors(bgColor))
+
+function bgColors(color: string) {
+  switch (color) {
+    case 'primary':
+      return 'bg-blue-400'
+    case 'secondary':
+      return 'bg-gray-400'
+    case 'danger':
+      return 'bg-red-400'
+    default:
+      return 'bg-green-400'
+  }
+}
+</script>
+
+<template>
+  <button class="rounded text-white py-2 px-4" :class="[backgroundColor]">
+    {{ label }}
+  </button>
+</template>
