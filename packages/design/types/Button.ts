668d025d9b7485268e84624b8eb5a0a06efaf5de
Develing1991 <completed0728@gmail.com>
2023-04-17 15:38:44 +0900

    types폴더로 분리

---
+++ packages/design/types/Button.ts
@@ -0,0 +1,6 @@
+// props type... issue nuxt macro를 사용할지.. 최선의 방법 고민 좀...
+// https://github.com/vuejs/core/issues/4294#issuecomment-1364769027
+export interface Props {
+  label: string
+  bgColor: string
+}
