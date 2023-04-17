618437db0427f5b7f69a185617861f34a758e74a
Develing1991 <completed0728@gmail.com>
2023-04-17 18:21:24 +0900

    vite-plugin-vue-type-imports 테스트

---
+++ packages/design/components/button/ButtonSetupType.vue
@@ -0,0 +1,32 @@
+<!-- https://github.com/wheatjs/vite-plugin-vue-type-imports -->
+<!-- vite.config.ts, plugins:[VueTypeImports()] -->
+<script setup lang="ts">
+import type { Props } from '@/types/Button'
+
+const { label, bgColor } = withDefaults(defineProps<Props>(), {
+  label: 'Button',
+  bgColor: 'bg-green-400',
+  reverse: false,
+})
+
+const backgroundColor = computed(() => bgColors(bgColor))
+
+function bgColors(color: string) {
+  switch (color) {
+    case 'primary':
+      return 'bg-blue-400'
+    case 'secondary':
+      return 'bg-gray-400'
+    case 'danger':
+      return 'bg-red-400'
+    default:
+      return 'bg-green-400'
+  }
+}
+</script>
+
+<template>
+  <button class="rounded text-white py-2 px-4" :class="[backgroundColor]">
+    {{ label }}
+  </button>
+</template>
--- packages/design/vite.config.ts
+++ packages/design/vite.config.ts
@@ -6,10 +6,14 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://github.com/wheatjs/vite-plugin-vue-type-imports
+import VueTypeImports from 'vite-plugin-vue-type-imports'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueTypeImports(),
     AutoImport({
       imports: ['vue'], // "vue-router"
       dirs: ['./composables', './hooks'],
