9e4a13d5ba7e506ae9ab38b2c59d29d1962babf7
Develing1991 <completed0728@gmail.com>
2023-04-17 18:21:50 +0900

    defineComponent로 스토리북 테스트

---
+++ packages/design/components/button/ButtonDefine.vue
@@ -0,0 +1,45 @@
+<template>
+  <button class="rounded text-white py-2 px-4" :class="[backgroundColor]">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import type { Props } from '@/types/Button'
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String as PropType<string>,
+      default: 'Button',
+    },
+    bgColor: {
+      type: String as PropType<string>,
+      default: 'bg-green-400',
+    },
+    reverse: {
+      type: Boolean as PropType<boolean>,
+      default: false,
+    },
+  },
+  setup(props: Props) {
+    const backgroundColor = computed(() => bgColors(props.bgColor))
+
+    function bgColors(color: string) {
+      switch (color) {
+        case 'primary':
+          return 'bg-blue-400'
+        case 'secondary':
+          return 'bg-gray-400'
+        case 'danger':
+          return 'bg-red-400'
+        default:
+          return 'bg-green-400'
+      }
+    }
+    return {
+      backgroundColor,
+    }
+  },
+})
+</script>
