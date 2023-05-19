5d5681e1582eee30acd2f47c00c7ec5f5c6afc3d
shlee ⚡️ <completed0728@gmail.com>
2023-05-19 05:40:18 +0000

    feat: add default header component

---
+++ packages/webapp/components/layout/header/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <FrameHeader>
+    <template #left>
+      <h1>
+        MyShop
+      </h1>
+    </template>
+    <template #center>
+      <!-- <div>center</div> -->
+    </template>
+    <template #right>
+      <div>
+        right
+      </div>
+    </template>
+  </FrameHeader>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+
+}
+</style>
