f5b49622e012c4e69ba4790e41c355f504a92e00
shlee ⚡️ <completed0728@gmail.com>
2023-05-19 05:38:22 +0000

    feat: add header frame slot

---
+++ packages/webapp/components/frame/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div>
+      <slot name="left" />
+    </div>
+    <div>
+      <slot name="center" />
+    </div>
+    <div>
+      <slot name="right" />
+    </div>
+  </header>
+</template>
+
+  <style lang="scss" scoped>
+  header{
+      @apply flex justify-between px-2
+  }
+  </style>
