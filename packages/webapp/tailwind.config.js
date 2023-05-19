93ac8a65e2871101df32bd38a930da16cc36b497
shlee ⚡️ <completed0728@gmail.com>
2023-05-19 05:37:21 +0000

    fix: add typography

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@develing1991/design/**/*.vue",
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
+    './node_modules/@develing1991/design/**/*.vue',
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+      //   roboto: ['Roboto'],
+      //   play: ['Play'],
+      //   noto: ['Noto Sans Nandinagari'],
+      // },
+    },
   },
-  plugins: [],
-};
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
